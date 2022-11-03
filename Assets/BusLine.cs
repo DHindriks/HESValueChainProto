85563858bd9d52753bf7f93bdd4ee329949726b3
DHindriks <dl.hindriks@gmail.com>
2022-11-03 17:22:11 +0100

    Proto route lines

---
+++ Assets/BusLine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusLine : MonoBehaviour
+{
+    [SerializeField] int BusNumber;
+    [SerializeField] Color LineColor;
+    [SerializeField] List<BusStop> busStopsOnRoute;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LineRenderer line = GetComponent<LineRenderer>();
+        List<Vector3> StopPositions = new List<Vector3>();
+        line.startColor = LineColor;
+        line.endColor = LineColor;
+
+        foreach (BusStop stop in busStopsOnRoute) 
+        {
+            StopPositions.Add(stop.transform.position);
+        }
+
+
+        line.positionCount = StopPositions.Count;
+        line.SetPositions(StopPositions.ToArray());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
