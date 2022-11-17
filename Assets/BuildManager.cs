47b5f1af3d2b72f35edfb101b1c3bdede9924a4e
DHindriks <dl.hindriks@gmail.com>
2022-11-17 17:00:21 +0100

    Proto build system

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public void InstatiateBuildingPrefab(GameObject buildingPrefab)
+    {
+        Instantiate(buildingPrefab);
+    }
+
+}
---
+++ Assets/BuildPlacer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPlacer : MonoBehaviour
+{
+    [SerializeField] LayerMask mask; //TODO: fix mask not working
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, mask))
+        {
+            transform.position = hit.point;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Destroy(this);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
