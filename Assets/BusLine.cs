9f735ce74976d4941f3661c7e256c2f55370d2a6
DHindriks <dl.hindriks@gmail.com>
2023-01-19 15:55:38 +0100

    Added the rest of the bus stops, added the first 7 bus lines, user can change bus line view settings

--- Assets/BusLine.cs
+++ Assets/BusLine.cs
@@ -6,6 +6,8 @@ public class BusLine : MonoBehaviour
 {
     public int BusNumber;
     public Color LineColor;
+    Color FuelColor;
+
     [SerializeField] List<BusStop> busStopsOnRoute;
 
     public BusStorage LineStorage;
@@ -18,13 +20,14 @@ public class BusLine : MonoBehaviour
 
     LineRenderer line;
 
+    [HideInInspector] public BuslineManager Manager;
+
     // Start is called before the first frame update
     void Start()
     {
         List<Vector3> StopPositions = new List<Vector3>();
         line = GetComponent<LineRenderer>();
-        line.startColor = LineColor;
-        line.endColor = LineColor;
+        RefreshDisplay();
 
         foreach (BusStop stop in busStopsOnRoute) 
         {
@@ -48,6 +51,58 @@ public class BusLine : MonoBehaviour
     public void ChangeFuelType(FuelTypes type)
     {
         Fueltype = type;
+
+        RefreshDisplay();
+    }
+
+    public void RefreshDisplay()
+    {
+
+        if (Fueltype == FuelTypes.Diesel)
+        {
+            FuelColor = Manager.FossilFuel;
+        }
+        else if (Fueltype == FuelTypes.Elektrisch)
+        {
+            FuelColor = Manager.ElectricFuel;
+        }
+        else if (Fueltype == FuelTypes.Waterstof)
+        {
+            FuelColor = Manager.HydrogenFuel;
+        }
+
+        switch (Manager.CurrentDispSetting)
+        {
+            case BuslineManager.BuslineDisplay.Buslines:
+                line.material = Manager.SimpleColorBase;
+                line.startColor = LineColor;
+                line.endColor = LineColor;
+                break;
+
+            case BuslineManager.BuslineDisplay.Fueltypes:
+                line.material = Manager.SimpleColorBase;
+                line.startColor = FuelColor;
+                line.endColor = FuelColor;
+                break;
+
+            case BuslineManager.BuslineDisplay.BuslinesFueltypes:
+                switch (Fueltype)
+                {
+                    case FuelTypes.Diesel:
+                        line.material = Manager.FossilCombo;
+                        break;
+                    case FuelTypes.Elektrisch:
+                        line.material = Manager.ElectricCombo;
+                        break;
+                    case FuelTypes.Waterstof:
+                        line.material = Manager.HydrogenCombo;
+                        break;
+                }
+                line.startColor = LineColor;
+                line.endColor = LineColor;
+                break;
+        }
+
     }
 
     public void SetStorage(BusStorage NewStorage)
---
+++ Assets/BuslineManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuslineManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI OptionDisplay;
+    public List<BusLine> Buslines;
+
+    public enum BuslineDisplay
+    {
+        Buslines,
+        Fueltypes,
+        BuslinesFueltypes
+    }
+    public BuslineDisplay CurrentDispSetting = BuslineDisplay.Buslines;
+
+    [Space(5), Tooltip("Single Display mats")]
+    public Material SimpleColorBase;
+    public Color HydrogenFuel;
+    public Color ElectricFuel;
+    public Color FossilFuel;
+
+    [Space(5), Tooltip("Combined Display mats")]
+    public Material HydrogenCombo;
+    public Material ElectricCombo;
+    public Material FossilCombo;
+
+    void Start()
+    {
+        foreach(BusLine line in Buslines)
+        {
+            line.Manager = this;
+        }
+    }
+
+    public void SetNewOption()
+    {
+        switch(OptionDisplay.text)
+        {
+            case "Buslijnen":
+                CurrentDispSetting = BuslineDisplay.Buslines;
+                break;
+
+            case "Brandstoftypes":
+                CurrentDispSetting = BuslineDisplay.Fueltypes;
+                break;
+
+            case "Gecombineerd":
+                CurrentDispSetting = BuslineDisplay.BuslinesFueltypes;
+                break;
+        }
+
+        foreach (BusLine line in Buslines)
+        {
+            line.RefreshDisplay();
+        }
+    }
+
+}
